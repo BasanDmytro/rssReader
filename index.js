b859136846e36b875fc9f0d09deb9233f356920c
Dmitriy Basan <basandima@gmail.com>
2017-02-05 15:41:37 +0200

    select rss and build rss table

--- index.js
+++ index.js
@@ -1,59 +1,57 @@
-
-
-//var xhr = new XMLHttpRequest();
-//xhr.open("GET", "https://www.flickr.com/services/feeds/photos_public.gne?id=33616145@N00&lang=en-us&format=atom", true);
-//xhr.onreadystatechange = function() {
- //   if (xhr.readyState == 4) {
-        // innerText does not let the attacker inject HTML elements.
-//      console.log(xhr.responseXML);
-  //      var xmlDoc = xhr.responseXML;
-  //      var markers = xmlDoc.getElementsByTagName("entry") ;
-  //      console.log(markers);
-  //  }
-
-//};
-//xhr.send();
-    var items = [];
-
-    $.ajax({
-        type: 'GET',
-        url: 'https://www.flickr.com/services/feeds/photos_public.gne?id=33616145@N00&lang=en-us&format=atom',
-        dataType: 'xml',
-        error: function(xhr) {
-            alert('Failed to parse feed');
-        },
-        success: function(xml) {
-            var channel = $('entry', xml).eq(0);
-            $('entry', xml).each( function() {
-                var item = {};
-                item.title = $(this).find('title').eq(0).text();
-                item.id = $(this).find('id').eq(0).text();
-                item.published = $(this).find('published').eq(0).text();
-                item.updated = $(this).find('updated').eq(0).text();
-                item.authorName = $(this).find('name').text();
-                item.authorUri = $(this).find('uri').text();
-                item.content= $(this).find('content').text();
-                items.push(item);
-                console.dir(item);
-            });
-            buildRSS(items);
+ function parseRSS() {
+        var form = document.forms[0];
+        var select = form.elements.url;
+        var urlSelected;
+        for (var i = 0; i < select.options.length; i++) {
+            var option = select.options[i];
+            if(option.selected) {
+                urlSelected = option.value;
+            }
         }
-    });
+        parseXML(urlSelected);
+    }
+
+    function parseXML(url) {
+        $.ajax({
+            type: 'GET',
+            url: url,
+            dataType: 'xml',
+            error: function(xhr) {
+                alert('Failed to parse feed');
+            },
+            success: function(xml) {
+                var channel = $('entry', xml).eq(0);
+                var items = [];
+                $('entry', xml).each( function() {
+                    var item = {};
+                    item.title = $(this).find('title').eq(0).text();
+                    item.id = $(this).find('id').eq(0).text();
+                    item.published = $(this).find('published').eq(0).text();
+                    item.updated = $(this).find('updated').eq(0).text();
+                    item.authorName = $(this).find('name').text();
+                    item.authorUri = $(this).find('uri').text();
+                    item.content= $(this).find('content').text();
+                    items.push(item);
+                    console.dir(item);
+                });
+                buildRSS(items);
+            }
+        });
+    }
 
     function buildRSS(temp) {
-        items = temp;
+        var items = temp;
         console.log(items[0].authorName);
         console.log(items.length);
         var table = document.getElementById('rss');
         table.setAttribute('width','auto');
         table.setAttribute('border','1');
-
         for (var i = 0; i < items.length; i++) {
             var newRow = table.insertRow(i);
-
             var newCell0 = newRow.insertCell(0);
             newCell0.width = window.innerWidth;
-            newCell0.innerHTML = items[i].authorName + items[i].content;
+            var date = new Date(items[i].published);
+            newCell0.innerHTML = "<b>" + items[i].title + "</b>" + "<hr>" + date + "<br>" + items[i].content;
         }
     }
 
