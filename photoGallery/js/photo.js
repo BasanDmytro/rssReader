e6e3b01ebd7a49461f0bc53b8169259b75acc197
Dmitriy Basan <basandima@gmail.com>
2017-02-07 20:08:24 +0200

    add slide to left for gallery

--- photoGallery/js/photo.js
+++ photoGallery/js/photo.js
@@ -75,8 +75,8 @@ var ImageView = Backbone.View.extend({
         this.render();
     },
     render: function () {
-        _.each(this.collection.models, function (item) { // проход по колекции моделей
-            if (item.get('visibleImage') == 1) { //проверка видно или нет
+        _.each(this.collection.models, function (item) {
+            if (item.get('visibleImage') == 1) {
                 this.$el.html('<img src="' + item.get('uri') + '" + id="bigImage" />');
             }
         }, this);
@@ -86,32 +86,16 @@ var ImageView = Backbone.View.extend({
 
 var PreviewOne = Backbone.View.extend({
     initialize: function () {
-        images.models[0].setPrev();
-        images.models[1].setPrev();
-        images.models[2].setPrev();
         this.collection = images;
-        console.log(this.collection);
         this.render();
 
     },
     render: function () {
-        var imgs = [];
-        var i1 = 1;
-        var i2 = 2;
         var i3 = 3;
 
-        _.each(this.collection.models, function (item) {
-            if (item.getPreview() == 1) {
-                var contactView = new ContactView({
-                    model: item
-                });
-                console.log(contactView);
-                imgs.push(contactView);
-            }
-        }, this);
-        document.getElementById('smallImage1').innerHTML = ('<img src="' + imgs[0].model.get('uri') + '"  />');
-        document.getElementById('smallImage2').innerHTML = ('<img src="' + imgs[1].model.get('uri') + '"  />');
-        document.getElementById('smallImage3').innerHTML = ('<img src="' + imgs[2].model.get('uri') + '"  />');
+        document.getElementById('smallImage1').innerHTML = ('<img src="' + images.models[i3-3].get('uri') + '"  />');
+        document.getElementById('smallImage2').innerHTML = ('<img src="' + images.models[i3-2].get('uri') + '"  />');
+        document.getElementById('smallImage3').innerHTML = ('<img src="' + images.models[i3-1].get('uri') + '"  />');
 
         document.getElementById('smallImage1').addEventListener( "click", (function () {
             document.getElementById('bigImage').innerHTML = document.getElementById('smallImage1').innerHTML
@@ -125,29 +109,22 @@ var PreviewOne = Backbone.View.extend({
         }) );
         document.getElementById('rightButton').addEventListener( "click", (function () {
             if (i3 < images.models.length) {
-                imgs[0].model.setUri(imgs[1].model.get('uri'));
-                imgs[1].model.setUri(imgs[2].model.get('uri'));
-                imgs[2].model.setUri(images.models[i3].getUri());
-                document.getElementById('smallImage3').innerHTML = ('<img src="' + imgs[2].model.get('uri') + '"  />');
-                document.getElementById('smallImage2').innerHTML = ('<img src="' + imgs[1].model.get('uri') + '"  />');
-                document.getElementById('smallImage1').innerHTML = ('<img src="' + imgs[0].model.get('uri') + '"  />');
+                document.getElementById('smallImage3').innerHTML = ('<img src="' + images.models[i3].getUri() + '"  />');
+                document.getElementById('smallImage2').innerHTML = ('<img src="' + images.models[i3-1].getUri() + '"  />');
+                document.getElementById('smallImage1').innerHTML = ('<img src="' + images.models[i3-2].getUri() + '"  />');
                 i3++;
             }
         }));
         document.getElementById('leftButton').addEventListener( "click", (function () {
             if (i3 > 3) {
-                imgs[2].model.setUri(imgs[1].model.get('uri'));
-                imgs[1].model.setUri(imgs[0].model.get('uri'));
-                imgs[0].model.setUri(images.models[i3-4].getUri());
-                document.getElementById('smallImage3').innerHTML = ('<img src="' + imgs[2].model.get('uri') + '"  />');
-                document.getElementById('smallImage2').innerHTML = ('<img src="' + imgs[1].model.get('uri') + '"  />');
-                document.getElementById('smallImage1').innerHTML = ('<img src="' + imgs[0].model.get('uri') + '"  />');
+                document.getElementById('smallImage3').innerHTML = ('<img src="' + images.models[i3 - 2].getUri() + '"  />');
+                document.getElementById('smallImage2').innerHTML = ('<img src="' + images.models[i3 - 3].getUri() + '"  />');
+                document.getElementById('smallImage1').innerHTML = ('<img src="' + images.models[i3 - 4].getUri() + '"  />');
                 i3--;
             }
         }));
     }
 });
-
 
 
 var directory = new ImageView();
